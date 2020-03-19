2d112952d609470c73de39a871fa58f4ef071fa7
Stuart Mathews <stuart@stuartmathews.com>
2020-03-19 12:05:17 +0000

    remove players command manager

--- MazerPlatformer/Player.cs
+++ MazerPlatformer/Player.cs
@@ -16,7 +16,6 @@ namespace MazerPlatformer
     public class Player : Character
     {
         public const string PlayerId = "Player";
-        private readonly CommandManager _playerCommands = CommandManager.GetInstance();
 
         public Player(int x, int y, int w, int h, AnimationInfo animationInfo) : base(x, y, PlayerId, w, h, GameObjectType.Player) 
             => AnimationInfo = animationInfo;
@@ -26,20 +25,19 @@ namespace MazerPlatformer
             base.Initialize();
             AddComponent(ComponentType.Health, 100); // start off with 100 health
             AddComponent(ComponentType.Points, 0); // start off with 0 points
+
             // Get notified when I collide with another object (collision handled in base class)
             OnCollision += HandleCollision;
         }
-
-
-        // I can update myself
+        
+        // I can update myself!
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
-            _playerCommands.Update(gameTime);
             Animation.Update(gameTime, (int)GetCentre().X, (int)GetCentre().Y);
         }
 
-        // I can draw myself
+        // I can draw myself!
         public override void Draw(SpriteBatch spriteBatch)
         {
             Animation.Draw(spriteBatch);
@@ -56,26 +54,24 @@ namespace MazerPlatformer
             NudgeOutOfCollision();
 
             // Change my health component to be affected by the hitpoints of the other object
-            if (otherObject.Type == GameObjectType.Npc)
+            if (otherObject.Type != GameObjectType.Npc) return;
+
+            var npcTypeComponent = otherObject.FindComponentByType(ComponentType.NpcType);
+            var npcType = (Npc.NpcTypes)npcTypeComponent.Value;
+
+            if (npcType == Npc.NpcTypes.Enemy)
             {
-                var npcTypeComponent = otherObject.FindComponentByType(ComponentType.NpcType);
-                var npcType = (Npc.NpcTypes)npcTypeComponent.Value;
+                var hitPoints = otherObject.FindComponentByType(ComponentType.HitPoints).Value;
+                var myHealth = FindComponentByType(ComponentType.Health).Value;
+                UpdateComponentByType(ComponentType.Health, (int) myHealth - (int) hitPoints);
+            }
 
-                if (npcType == Npc.NpcTypes.Enemy)
-                {
-                    var hitPoints = otherObject.FindComponentByType(ComponentType.HitPoints).Value;
-                    var myHealth = FindComponentByType(ComponentType.Health).Value;
-                    UpdateComponentByType(ComponentType.Health, (int) myHealth - (int) hitPoints);
-                }
-
-                if (npcType == Npc.NpcTypes.Pickup)
-                {
-                    var pickupPoints = (int) otherObject.FindComponentByType(ComponentType.Points).Value;
-                    var myPoints = (int)FindComponentByType(ComponentType.Points).Value;
-                    var levelPoints = myPoints + pickupPoints;
-                    
-                    UpdateComponentByType(ComponentType.Points, levelPoints);
-                }
+            if (npcType == Npc.NpcTypes.Pickup)
+            {
+                var pickupPoints = (int) otherObject.FindComponentByType(ComponentType.Points).Value;
+                var myPoints = (int)FindComponentByType(ComponentType.Points).Value;
+                var levelPoints = myPoints + pickupPoints;
+                UpdateComponentByType(ComponentType.Points, levelPoints);
             }
         }
     }
