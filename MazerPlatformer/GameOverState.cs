6be214cb2ecb25f0e4f54a1a4531eb71afe4ad53
Stuart Mathews <stuart@stuartmathews.com>
2020-03-25 14:13:52 +0000

    add gameOver state - not using really

---
+++ MazerPlatformer/GameOverState.cs
@@ -0,0 +1,13 @@
+﻿using GameLibFramework.FSM;
+
+namespace MazerPlatformer
+{
+    public class GameOverState : State
+    {
+        private Mazer game;
+        public GameOverState(Mazer game) : base("GameOverState")
+        {
+            this.game = game;
+        }
+    }
+}
