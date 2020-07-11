6c702d8ed1e004f57de9099d3aa017f29c7d9e1c
Stuart Mathews <stumathews@gmail.com>
2020-07-12 00:07:53 +0100

    FP-15: move out classes into own files

--- MazerPlatformer/NPC.cs
+++ MazerPlatformer/NPC.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,7 +7,6 @@ using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using GameLibFramework.Animation;
-using GameLibFramework.FSM;
 using LanguageExt;
 using static MazerPlatformer.Statics;
 
@@ -50,148 +48,6 @@ namespace MazerPlatformer
         {
             NpcState = NpcStates.Colliding;
             return otherObject.IsPlayer() ? Nothing : NudgeOutOfCollision();
-        }
-    }
-
-    public class NpcState : State
-    {
-        protected float WaitTime;
-        protected Npc Npc { get; set; }
-        public NpcState(string name, Npc Npc) : base(name) => this.Npc = Npc;
-
-        protected bool IsWithin(int milli, GameTime dt)
-        {
-            var isWithin = false;
-            if(WaitTime < milli)
-            {
-                isWithin = true;
-                WaitTime += dt.ElapsedGameTime.Milliseconds;
-            }
-            else
-            {
-                WaitTime = 0;
-            }
-            return isWithin;
-        }
-    }
-
-    public class MovingState : NpcState
-    {
-        public MovingState(string name, Npc npc) : base(name, npc) {}
-
-        private readonly SimpleGameTimeTimer _spottedPlayerTimeout = new SimpleGameTimeTimer(5000);
-        public override void Enter(object owner)
-        {
-            base.Enter(owner);
-            Npc.InfoText = "M";
-            _spottedPlayerTimeout.Start();
-        }
-
-        public override void Update(object owner, GameTime gameTime)
-        {
-            base.Update(owner, gameTime);
-            _spottedPlayerTimeout.Update(gameTime);
-
-            var x =
-                from playerComponent in Npc.FindComponentByType(Component.ComponentType.Player)
-                select Npc.FindComponentByType(Component.ComponentType.GameWorld)
-                    .Map(component => (GameWorld) component.Value)
-                    .Iter(gameWorld =>
-                    {
-                        var player = (Player) playerComponent.Value;
-                        // Great example of using an Option<Room> to get an expected room, otherwise throw as unrecoverable
-                        var npcRoom = gameWorld.GetRoomIn(Npc).ThrowIfNone(NotFound.Create("Room unexpectedly not found"));
-                        var myRow = gameWorld.ToRoomRow(Npc).ThrowIfNone(NotFound.Create("Could not find room row for NPC"));
-                        var myCol = gameWorld.ToRoomColumn(Npc).ThrowIfNone(NotFound.Create("Could not find room col for NPC"));
-                        var playerRow = gameWorld.ToRoomRow(player).ThrowIfNone(NotFound.Create("Could not find room row for player"));
-                        var playerCol = gameWorld.ToRoomColumn(player).ThrowIfNone(NotFound.Create("Could not find room col for player"));
-                        var sameRow = playerRow == myRow;
-                        var sameCol = playerCol == myCol;
-                        Npc.SubInfoText = $"R={myRow} C={myCol}";
-                        player.SubInfoText = $"R={playerRow}C={playerCol}";
-
-                        var playerSeen = false;
-                        if (Npc.BoundingSphere.Intersects(npcRoom.BoundingSphere))
-                        {
-                            Character.CharacterDirection newDir;
-                            var changeDirection = !sameCol || !sameRow;
-                            if (sameCol && gameWorld.IsPathAccessibleBetween(player, Npc).ThrowIfFailed())
-                            {
-                                newDir = myRow < playerRow
-                                    ? Character.CharacterDirection.Down
-                                    : Character.CharacterDirection.Up;
-                                if (changeDirection)
-                                    Npc.ChangeDirection(newDir);
-                                playerSeen = true;
-
-                            }
-                            else if (sameRow && gameWorld.IsPathAccessibleBetween(player, Npc).ThrowIfFailed())
-                            {
-                                newDir = myCol < playerCol
-                                    ? Character.CharacterDirection.Right
-                                    : Character.CharacterDirection.Left;
-                                if (changeDirection)
-                                    Npc.ChangeDirection(newDir);
-                                playerSeen = true;
-                            }
-                        }
-
-                        if (playerSeen && _spottedPlayerTimeout.IsTimedOut())
-                        {
-                            player.Seen();
-                            _spottedPlayerTimeout.Reset();
-                        }
-
-                        Npc.MoveInDirection(Npc.CurrentDirection, gameTime);
-                    }).ToSuccess();
-        }
-    }
-
-    public class CollidingState : NpcState
-    {
-        public CollidingState(string name, Npc npc) : base(name, npc) { }
-
-        public override void Enter(object owner)
-        {
-            base.Enter(owner);
-            Npc.InfoText = "C";
-            Npc.SetAsIdle();
-
-        }
-
-        public override void Update(object owner, GameTime gameTime)
-        {
-            base.Update(owner, gameTime);
-            Npc.NpcState = Npc.NpcStates.Deciding;
-        }
-    }
-
-    public class DecisionState : NpcState
-    {
-        public DecisionState(string name, Npc npc) : base(name, npc) {}
-        public override void Enter(object owner)
-        {
-            
-            Npc.InfoText = "D";
-            Npc.CanMove = false;
-            Npc.SetAsIdle();
-        }
-
-        public override void Update(object owner, GameTime gameTime)
-        {
-            base.Update(owner, gameTime);
-
-            // skip doing anything for a few secs
-            if (IsWithin(100, gameTime))
-                return;
-
-           
-            Npc.SwapDirection();
-            
-
-            // then move
-            if (!Npc.IsColliding)
-                Npc.NpcState = Npc.NpcStates.Moving;
         }
     }
 }
