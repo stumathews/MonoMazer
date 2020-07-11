64d0a208d63221abe230ddb7e8b72bd924268a38
Stuart Mathews <stumathews@gmail.com>
2020-07-11 19:27:34 +0100

    FP-15: Create IFailure from concrete ConditionNotSatisfied clas

--- MazerPlatformer/ConditionNotSatisfied.cs
+++ MazerPlatformer/ConditionNotSatisfied.cs
@@ -7,5 +7,6 @@
             Reason = $"Condition not satisfied. Caller: {caller}";
         }
         public string Reason { get; set; }
+        public static IFailure Create(string caller) => new ConditionNotSatisfied(caller);
     }
 }
