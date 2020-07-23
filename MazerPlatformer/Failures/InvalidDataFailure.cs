d6d8fe7dff086233e527c583851c4e16cd5caaff
Stuart Mathews <stumathews@gmail.com>
2020-07-23 22:02:36 +0100

    FP-15: new InvalidDataFailure Failure type

---
+++ MazerPlatformer/Failures/InvalidDataFailure.cs
@@ -0,0 +1,13 @@
+﻿namespace MazerPlatformer
+{
+    public class InvalidDataFailure : IFailure
+    {
+        public InvalidDataFailure(string empty)
+        {
+            Reason = empty;
+        }
+
+        public string Reason { get; set; }
+        public static IFailure Create(string message) => new InvalidDataFailure(message);
+    }
+}
