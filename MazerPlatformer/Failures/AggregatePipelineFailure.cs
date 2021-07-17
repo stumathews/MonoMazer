872fbe17fb088b12a5ccb9d4781d981e59eb48ed
Stuart Mathews <stumathews@gmail.com>
2021-07-17 22:23:42 +0100

    add frequencies member to help with testing

--- MazerPlatformer/Failures/AggregatePipelineFailure.cs
+++ MazerPlatformer/Failures/AggregatePipelineFailure.cs
@@ -8,16 +8,22 @@ namespace MazerPlatformer
     {
         public AggregatePipelineFailure(IEnumerable<IFailure> failures)
         {
+            frequencies = new Dictionary<IFailure, int>();
             var failureNames = failures.GroupBy(o => o.GetType().Name + o.Reason);
             var sb = new StringBuilder();
             foreach (var name in failureNames)
             {
+                var lookupFailure = failures.First(o=>(o.GetType().Name + o.Reason).Equals(name.Key));
+                if(!frequencies.ContainsKey(lookupFailure))
+                    frequencies.Add(lookupFailure, name.Count());
+
                 sb.Append($"Failure name: {name.Key} Count: {name.Count()}\n");
             }
 
             Reason = sb.ToString();
         }
         public string Reason { get; set; }
+        public Dictionary<IFailure, int> frequencies {get;set;}
         public static IFailure Create(IEnumerable<IFailure> failures) => new AggregatePipelineFailure(failures);
     }
 }
